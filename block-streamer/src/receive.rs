a174fd66eb63dc8b2fd67998d1184b60da77ba27
Ryan Plauche <ryan.plauche@gmail.com>
2023-01-06 10:05:57 -0600

    Hacky wrapping with cbor which allows out-of-order reassembly

--- block-streamer/src/receive.rs
+++ block-streamer/src/receive.rs
@@ -1,5 +1,7 @@
 use anyhow::{anyhow, Result};
 use cid::Cid;
+use serde_cbor_2::Deserializer;
+use std::collections::BTreeMap;
 use std::net::SocketAddr;
 use std::path::PathBuf;
 use std::thread::sleep;
@@ -11,20 +13,63 @@ use tokio::net::UdpSocket;
 struct DataBlob {
     pub cid: Cid,
     pub data: Vec<u8>,
+    pub links: Vec<Cid>,
 }
 
 impl DataBlob {
     pub fn try_parse(packet: &[u8]) -> Result<Self> {
         if packet.len() > 36 {
-            let (cid, data) = packet.split_at(36);
+            // let mut decoder = Decoder::new(&packet);
+            // let map = decoder.map().unwrap();
+            // let cid = decoder.bytes().unwrap();
+            // let data = decoder.bytes().unwrap();
+            let mut deser = Deserializer::from_slice(&packet);
+            let datamap: BTreeMap<&str, Vec<Vec<u8>>> =
+                serde::de::Deserialize::deserialize(&mut deser).unwrap();
+            let cid = datamap.get("cid").unwrap().clone().pop().unwrap();
+            let data = datamap.get("data").unwrap();
+            let raw_links = datamap.get("links").unwrap().clone();
+
+            let mut links = vec![];
+            for link in raw_links {
+                links.push(Cid::try_from(link)?);
+            }
+
+            // let (cid, data) = packet.split_at(36);
             Ok(DataBlob {
                 cid: Cid::try_from(cid)?,
-                data: data.to_vec(),
+                data: data.first().unwrap().to_vec(),
+                links,
             })
         } else {
             Err(anyhow!("Not enough data"))
         }
     }
+}
+
+async fn assemble(path: &PathBuf, blocks: Vec<DataBlob>) -> Result<()> {
+    // TODO: This is pretty dumb and provides no verification that these blocks belong together
+    // or are in the correct order. First we probably need to transmit something closer to an
+    // actual DAG structure with links, and then we can use that structure for verification
+    // and reassembly here.
+
+    let root = blocks.iter().find(|b| b.links.len() > 0);
+
+    if let Some(root) = root {
+        let mut output_file = File::create(path).await?;
+        for cid in &root.links {
+            if let Some(data) = blocks.iter().find(|b| b.cid == *cid) {
+                output_file.write_all(&data.data).await?;
+            }
+        }
+        output_file.flush().await?;
+    }
+
+    Ok(())
+
+    // for packet in blocks {
+    //     output_file.write_all(&packet.data).await?;
+    // }
 }
 
 pub async fn receive(path: &PathBuf, listen_addr: &String) -> Result<()> {
@@ -38,7 +83,7 @@ pub async fn receive(path: &PathBuf, listen_addr: &String) -> Result<()> {
 
     let mut data = vec![];
 
-    let mut buf = vec![0; 1024];
+    let mut buf = vec![0; 10240];
     let mut receiving_cid = false;
     loop {
         if let Ok(len) = socket.try_recv(&mut buf) {
@@ -59,15 +104,5 @@ pub async fn receive(path: &PathBuf, listen_addr: &String) -> Result<()> {
 
     println!("Received {} packets, writing file", data.len());
 
-    // TODO: This is pretty dumb and provides no verification that these blocks belong together
-    // or are in the correct order. First we probably need to transmit something closer to an
-    // actual DAG structure with links, and then we can use that structure for verification
-    // and reassembly here.
-    let mut output_file = File::create(path).await?;
-    for packet in data {
-        output_file.write_all(&packet.data).await?;
-    }
-    output_file.flush().await?;
-
-    Ok(())
+    Ok(assemble(path, data).await.expect("File assembly failed"))
 }
--- block-streamer/src/transmit.rs
+++ block-streamer/src/transmit.rs
@@ -1,12 +1,17 @@
 use anyhow::Result;
+use bytes::Bytes;
+use cid::Cid;
 use futures::TryStreamExt;
+use iroh_resolver::resolver::Block;
 use iroh_resolver::unixfs_builder::{File, FileBuilder};
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+use serde::{Serialize, Serializer};
+use serde_cbor_2::{to_vec, Value};
+use std::collections::BTreeMap;
 use std::net::SocketAddr;
 use std::path::PathBuf;
 use tokio::net::UdpSocket;
-
-// use rand::seq::SliceRandom;
-// use rand::thread_rng;
 
 async fn chunk(path: &PathBuf) -> Result<Vec<Vec<u8>>> {
     // TODO: This clearly chunks up the file and
@@ -19,25 +24,57 @@ async fn chunk(path: &PathBuf) -> Result<Vec<Vec<u8>>> {
         .fixed_chunker(24)
         .build()
         .await?;
-    let parts: Vec<_> = file.encode().await?.try_collect().await?;
+
+    let blocks: Vec<_> = file.encode().await?.try_collect().await?;
+
+    // let root = file.encode_root().await?;
+
+    // let parts = vec![root];
 
     let mut payloads = vec![];
 
-    for part in parts {
-        let mut payload = vec![];
-        payload.extend(part.cid().to_bytes());
-        payload.extend(part.data().to_vec());
-        payloads.push(payload);
+    for block in blocks {
+        // let mut payload = vec![];
+        // payload.extend(block.cid().to_bytes());
+        // payload.extend(block.data().to_vec());
+        // payloads.push(payload);
+        let mut datamap = BTreeMap::new();
+        datamap.insert("cid", vec![block.cid().to_bytes()]);
+        if block.links().len() > 0 {
+            datamap.insert("data", vec![vec![]]);
+        } else {
+            datamap.insert("data", vec![block.data().to_vec()]);
+        }
+
+        let mut links = vec![];
+        for l in block.links() {
+            links.push(l.to_bytes());
+        }
+        datamap.insert("links", links);
+
+        let buf = to_vec(&datamap).unwrap();
+        // let mut encoder = Encoder::new(&mut buf[..]);
+        // let res = encoder
+        //     .begin_map()
+        //     .unwrap()
+        //     .bytes(&block.cid().to_bytes())
+        //     .unwrap()
+        //     .bytes(&block.data())
+        //     .unwrap()
+        //     .end()
+        //     .unwrap();
+
+        payloads.push(buf);
     }
+
+    // This last part is the root block. I'm not sure what the part is
+    // but I can see the links to the other CIDs
+    // payloads.pop();
 
     // TODO: This randomly shuffles the order of parts in the payload vec.
     // Once we have verification and reassembly working correctly on the receiver side,
     // we should be able to shuffle the payload and still get the correct file on the other side.
-    // payloads.shuffle(&mut thread_rng());
-
-    // TODO: The last part is always too big and contains what looks like a bunch of
-    // garbage when I write it to the file...the current fix doesn't seem right haha
-    payloads.pop();
+    payloads.shuffle(&mut thread_rng());
 
     Ok(payloads)
 }
