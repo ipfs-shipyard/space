1f1d2dafc86189fa248c312c270d8cc841849d77
Ryan Plauche <ryan.plauche@gmail.com>
2023-01-26 14:40:05 -0600

    Added additional api commands and an app-api-cli for easily sending app api commands

--- block-streamer/src/control.rs
+++ block-streamer/src/control.rs
@@ -2,44 +2,49 @@ use anyhow::Result;
 
 use serde_json::from_slice;
 use std::net::SocketAddr;
+use std::path::PathBuf;
 use std::thread::sleep;
 use std::time::Duration;
 use tokio::net::UdpSocket;
 use tracing::info;
 
 use crate::api::ApplicationAPI;
+use crate::receive::receive;
+use crate::transmit::transmit;
 
 pub async fn control(listen_addr: &String) -> Result<()> {
     info!("Listening for control messages on {}", listen_addr);
     let listen_address: SocketAddr = listen_addr.parse()?;
-    let socket = UdpSocket::bind(&listen_address).await?;
 
     let mut buf = vec![0; 10240];
-
-    let val = ApplicationAPI::ImportFile("/dev/null".to_owned());
-    info!("Try this: {:?}", serde_json::to_string(&val));
+    let mut real_len;
 
     loop {
-        if let Ok(len) = socket.try_recv(&mut buf) {
-            if len > 0 {
-                if let Ok(msg) = from_slice::<ApplicationAPI>(&buf[..len]) {
-                    match msg {
-                        ApplicationAPI::ImportFile(file) => {
-                            info!("Received ImportFile: {}", file)
-                        }
-                        ApplicationAPI::ExportCid(cid) => {
-                            info!("Received ExportCid: {}", cid)
-                        }
-                        ApplicationAPI::IsConnected(status) => {
-                            info!("Received IsConnected: {}", status)
-                        }
-                        ApplicationAPI::IsCidComplete(cid) => {
-                            info!("Received ISCidComplete: {}", cid)
-                        }
+        {
+            let socket = UdpSocket::bind(&listen_address).await?;
+            loop {
+                if let Ok(len) = socket.try_recv(&mut buf) {
+                    if len > 0 {
+                        real_len = len;
+                        break;
                     }
+                }
+                sleep(Duration::from_millis(10));
+            }
+        }
+
+        if let Ok(msg) = from_slice::<ApplicationAPI>(&buf[..real_len]) {
+            match msg {
+                ApplicationAPI::Receive { path, listen_addr } => {
+                    receive(&PathBuf::from(path), &listen_addr).await?
+                }
+                ApplicationAPI::Transmit { path, target_addr } => {
+                    transmit(&PathBuf::from(path), &target_addr).await?
+                }
+                other => {
+                    info!("Received {:?}", other);
                 }
             }
         }
-        sleep(Duration::from_millis(10));
     }
 }
